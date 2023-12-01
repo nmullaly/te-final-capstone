d5c68dd494570a6ec158b2b790a8033a31e90396
Addie <Dylan.j.lubinsky@gmail.com>
2023-12-01 12:35:45 -0500

    did I break it?

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -3,9 +3,34 @@
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
+<body>
+  <header class = "header" id = "header">
+    <h1 id = "logo">Project Name Working Title</h1>
+    <button class="movies">movies</button>
+    <button class="users">users</button>
+    <button class="scoreboard">scoreboard</button>
+  </header>
+  <main>
+  <div class = "navigation">
+    Todo navigation
+  </div>
+  <div class = "movieDisplay">
+    Todo movieDisplay
+  </div>
+</main>
+  <footer class = "footer" id = "footer">Project Name Working Title Credits</footer>
+</body>
 </template>
 
 <script>
 export default {
 };
 </script>
+
+<style>
+.header{
+  display: flex;
+  background-color: 89A8E6;
+  border: 2px solid black;
+}
+</style> 
