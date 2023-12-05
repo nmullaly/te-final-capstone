db128e772507d9935b9c2c73ca9488519dc54684
Addie <Dylan.j.lubinsky@gmail.com>
2023-12-05 10:31:16 -0500

    test component got me feeling like 🫦

---
+++ vue/src/views/TestView.vue
@@ -0,0 +1,17 @@
+<template>
+    test view
+    <user-profile-pic></user-profile-pic>
+</template>
+
+<script>
+import UserProfilePic from "../components/UserProfilePic.vue"
+
+export default {
+components: {
+    UserProfilePic,
+}
+};
+</script>
+
+<style>
+</style>
