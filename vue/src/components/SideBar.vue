3e7d89c911978a6c979c9e42f83749f70bc5e58d
lovrekovic <josephlovrekovic@gmail.com>
2023-12-11 17:21:58 -0500

    pushing so i can pull

---
+++ vue/src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <b-button pill>Profile</b-button>
+</div>
+</template>
+<script>
+import profileService from '../services/ProfileService.js';
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/views/ProfileView.vue
+++ vue/src/views/ProfileView.vue
@@ -67,5 +67,7 @@ export default {
 </script>
 
 <style>
-
+.profile {
+	
+}
 </style>
